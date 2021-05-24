597e53e2e19798a25741e8067433c12a0ee24845
Ryan Zebian <zebian93@gmail.com>
2021-05-23 22:45:53 -0700

    add DockSetup and gpg git signing

---
+++ bin/setupDock.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+dockutil --remove FaceTime --no-restart
+dockutil --remove Calendar --no-restart
+dockutil --remove Contacts --no-restart
+dockutil --remove Reminders --no-restart
+dockutil --remove Notes --no-restart
+dockutil --remove TV --no-restart
+dockutil --remove Music --no-restart
+dockutil --remove Podcasts --no-restart
+dockutil --remove 'App Store' --no-restart
+dockutil --remove 'System Preferences'
