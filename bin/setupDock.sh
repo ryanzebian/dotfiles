e7bfc1ff9a6e3e52ac6af4a7b59731769fd2efa1
Ryan Zebian <zebian93@gmail.com>
2021-11-02 22:33:39 -0700

    setupDock: add apps and restart

--- bin/setupDock.sh
+++ bin/setupDock.sh
@@ -4,8 +4,15 @@ dockutil --remove Calendar --no-restart
 dockutil --remove Contacts --no-restart
 dockutil --remove Reminders --no-restart
 dockutil --remove Notes --no-restart
+dockutil --remove Messages --no-restart
+dockutil --remove Maps --no-restart
+dockutil --remove Photos --no-restart
+dockutil --remove News --no-restart
 dockutil --remove TV --no-restart
 dockutil --remove Music --no-restart
 dockutil --remove Podcasts --no-restart
 dockutil --remove 'App Store' --no-restart
+dockutil --remove Keynote --no-restart
+dockutil --remove Numbers --no-restart
+dockutil --remove Pages --no-restart
 dockutil --remove 'System Preferences'
