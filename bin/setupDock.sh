bdffe01463c460c20bf65860d208da267ea7fbb6
Ryan Zebian <zebian93@gmail.com>
2021-11-15 17:29:33 -0800

    dock: fix monteray support

--- bin/setupDock.sh
+++ bin/setupDock.sh
@@ -1,18 +1,18 @@
 #! /bin/bash
-dockutil --remove FaceTime --no-restart
-dockutil --remove Calendar --no-restart
-dockutil --remove Contacts --no-restart
-dockutil --remove Reminders --no-restart
-dockutil --remove Notes --no-restart
-dockutil --remove Messages --no-restart
-dockutil --remove Maps --no-restart
-dockutil --remove Photos --no-restart
-dockutil --remove News --no-restart
-dockutil --remove TV --no-restart
-dockutil --remove Music --no-restart
-dockutil --remove Podcasts --no-restart
-dockutil --remove 'App Store' --no-restart
-dockutil --remove Keynote --no-restart
-dockutil --remove Numbers --no-restart
-dockutil --remove Pages --no-restart
+dockutil --remove FaceTime 
+dockutil --remove Calendar 
+dockutil --remove Contacts 
+dockutil --remove Reminders 
+dockutil --remove Notes 
+dockutil --remove Messages 
+dockutil --remove Maps 
+dockutil --remove Photos 
+dockutil --remove News 
+dockutil --remove TV 
+dockutil --remove Music 
+dockutil --remove Podcasts 
+dockutil --remove 'App Store' 
+dockutil --remove Keynote 
+dockutil --remove Numbers 
+dockutil --remove Pages 
 dockutil --remove 'System Preferences'
